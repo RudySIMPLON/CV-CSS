fbf54e5ab6112caa531c22f6db1776b14ad895b8
simplon <rudy__10@hotmail.fr>
2017-01-01 22:08:36 +0100

    ajout des fichiers au depot

---
+++ app.js
@@ -0,0 +1,55 @@
+alert('Bienvenue ! sur ta To Do List, oublie pas de cliquer sur une tache pour la barré');
+console.log('voici mon texte afficher haha');
+
+$('.ui blue button button').keyup(function(e) { 
+	if(e.keyCode == 13) {
+		question = $('.input').val();
+		if ( question == ""){
+			$('ul').append();
+		} else {  
+			$('ul').append("<li>"+question+"</li>"); }
+		
+		}
+	});
+$('.button').click(function(){
+	question = $('.input').val();
+	if ( question == ""){
+		$('ul').append();
+	} else {  
+		$('ul').append("<li>"+question+"</li>"); }
+	 
+
+	});
+
+$('ul').on('click','li', function(){
+	$(this).toggleClass('done');
+
+});
+
+
+
+function effacer()
+{
+	document.getElementById('machin').value = '';
+}
+
+$("input").click(function(){
+
+	effacer();
+})
+
+
+$('.btn1').on('click',function(){
+$('li').show();
+$('li.done').show();
+});
+
+$('.btn2').on('click',function(){
+$('li').show();
+$('li.done').hide();
+});
+
+$('.btn3').on('click',function(){
+$('li').hide();
+$('li.done').show();
+});
